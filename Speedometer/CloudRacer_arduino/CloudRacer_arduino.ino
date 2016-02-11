fac7a6601a0277a82dbe6fd0510828e8aec3e020
Cloud Racer <Cloud Racer>
2016-02-11 11:38:57 -0500

    Updated Arudino code to work

--- Speedometer/CloudRacer_arduino/CloudRacer_arduino.ino
+++ Speedometer/CloudRacer_arduino/CloudRacer_arduino.ino
@@ -3,39 +3,59 @@
  Created:	1/31/2016 3:05:30 PM
  Author:	peter
 */
+const int REED_PIN = 7;
+const int RPM_INTERVAL = 50; //ms
+const int RPM_SAMPLES = 100;
 
+int prevReed;
+int latchTime = 2; // ms
+int reportInterval = 10; //ms
+uint32_t nextReadTime = millis();
+uint32_t nextReportTime = millis();
+uint32_t nextSampleTime = millis();
+volatile uint32_t rotations = 0;
+volatile uint32_t curRPM = 0;
+uint32_t readings[RPM_SAMPLES];
+int curReadingIndex = 0;
+
+  
 // the setup function runs once when you press reset or power the board
 void setup() {
-	pinMode(0, INPUT);
+	pinMode(REED_PIN, INPUT_PULLUP);
 	Serial.begin(1000000);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-	uint32_t rotations = 1;
-	int signal = 1;
-	uint16_t bpm = 1;
-	uint16_t ibi = 1;
-	bool pulse = true;
-	bool qs = true;
+  int reed = digitalRead(REED_PIN);
+  if ((reed != prevReed) && (millis() >= nextReadTime)) {
+    prevReed = reed;
+    nextReadTime = millis() + latchTime;
+    if (!reed) rotations++;
+  }
 
-	Serial.print("{ \"time\": ");
-	Serial.print(millis(), DEC);
-	Serial.print(", \"rotations\": ");
-	Serial.print(rotations, DEC);
-	Serial.print(", \"signal\": ");
-	Serial.print(signal, DEC);
-	Serial.print(", \"ibi\": ");
-	Serial.print(ibi, DEC);
-	Serial.print(", \"bpm\": ");
-	Serial.print(bpm, DEC);
-	Serial.print(", \"pulse\": ");
-	Serial.print(pulse, DEC);
-	Serial.print(", \"qs\": ");
-	Serial.print(qs, DEC);
-	Serial.println("}");
-	
-	delay(20);
+
+  if (millis() >= nextSampleTime)
+  {
+    uint32_t outgoingReading = readings[curReadingIndex];
+    readings[curReadingIndex] = rotations;
+    curRPM = (rotations - outgoingReading) * 6000 / RPM_INTERVAL / RPM_SAMPLES;
+    nextSampleTime += RPM_INTERVAL;
+
+    curReadingIndex++;
+    if (curReadingIndex > RPM_SAMPLES) curReadingIndex = 0;
+  }
+  
+  if (millis() >= nextReportTime) {
+  	Serial.print("{ \"time\": ");
+  	Serial.print(millis(), DEC);
+  	Serial.print(", \"rotations\": ");
+  	Serial.print(rotations, DEC);
+   Serial.print(", \"rpm\": ");
+    Serial.print(curRPM, DEC);
+  	Serial.println("}");
+    nextReportTime += reportInterval;
+  }
 }
 
 
